040014a6c36ff341c2e16a70a90ec7eb0d3f7bf9
devjuancarlos10 <136044770+devjuancarlos10@users.noreply.github.com>
2024-04-10 19:33:13 -0500

    Add files via upload

---
+++ secciones/pacientes/informacion-pacientes.php
@@ -0,0 +1,27 @@
+<?php
+include ("../secciones/pacientes.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/pacientes.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+    <header class="cabecera-paciente">
+        <div class="imagen-paciente">
+            <img src="../assets/interactua.svg" class="iconos-boton" alt="">
+        </div>
+        <p id="titulo-paciente">Pacientes</p>
+    </header>
+    
+       
+</body>
+
+</html>
